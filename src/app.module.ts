a935e9a1c2ceb2407f58bb28c3326a201347c5c9
SnakeVeryLong <ermolenko220699@gmail.com>
2022-06-07 10:22:47 +0700

    thirty

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { TShttpModule } from './modules/TS/TS-http.module';
+import { TSModule } from './modules/TS/TS.module';
 
 
 
@@ -17,7 +17,7 @@ import { TShttpModule } from './modules/TS/TS-http.module';
     entities: [],
     synchronize: true,
     autoLoadEntities: true
-  }), TShttpModule],
+  }), TSModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/TS/TS.module.ts
+++ src/modules/TS/TS.module.ts
@@ -1,10 +1,14 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TS } from "./entities/TS.entity";
+import { TSController } from "./TS.controller";
+import { TsService } from './ts/ts.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TS])],
   exports: [TypeOrmModule],
+  controllers: [TSController],
+  providers: [TsService],
 })
 
 export class TSModule {}
